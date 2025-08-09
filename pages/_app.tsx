bfb8a7299e8233b06123eac6d178e67a85604196
Jonathan Gomes <jonathan.dsg104@gmail.com>
2025-08-09 07:40:28 -0300

    feat(app): exibição global do carrinho fixo em todas as páginas, com comentários explicativos

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,18 @@
+
 import { Provider } from "react-redux";
 import { store } from "../store";
 import type { AppProps } from "next/app";
 import GlobalStyle from "../styles/global";
+import Cart from "../components/Cart";
 
+
+// Componente global do app: Provider Redux, estilos globais e carrinho fixo
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <GlobalStyle />
+      {/* Carrinho fixo à direita em todas as páginas */}
+      <Cart />
       <Component {...pageProps} />
     </Provider>
   );
