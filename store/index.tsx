4baf0bb6e20ea5866c0e6b9fa1bee237b612a631
Jonathan Gomes <jonathan.dsg104@gmail.com>
2025-08-09 07:33:48 -0300

    feat(store): configuração do store global com reducer do carrinho e comentários explicativos

--- store/index.tsx
+++ store/index.tsx
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
+import cartReducer from "../stores/slices/cartSlice";
 
+// Configuração do store global do Redux
+// Adicionando o reducer do carrinho
 export const store = configureStore({
   reducer: {
-    // Adicione seus reducers aqui
+    cart: cartReducer, // reducer do carrinho
   },
 });
 
+// Tipos para uso com hooks do Redux
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
